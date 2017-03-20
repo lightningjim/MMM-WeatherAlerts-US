f4359b8b4215e9b4e4e02b09c350e0426177f2f9
Kyle Creasey <kcreasey@so2.infinitysrv.com>
2017-03-19 19:07:33 -0500

    Test update for pulling data

--- MMM-WeatherAlerts-US.js
+++ MMM-WeatherAlerts-US.js
@@ -1,13 +1,84 @@
 Module.register("MMM-WeatherAlerts-US",{
     // Default module config.
     defaults: {
-        text: "Hello World!"
+		updateInterval: 10*60*1000, // 10 minutes
+		location: OKZ029, //Cleveland Co, Oklahoma
+        text: "TO WORK SOON"
     },
 
     // Override dom generator.
     getDom: function() {
         var wrapper = document.createElement("div");
+	
+		if (!this.loaded) {
+	      wrapper.innerHTML = this.translate('LOADING');
+	      return wrapper;
+		}
         wrapper.innerHTML = this.config.text;
         return wrapper;
     }
+
+	getScripts: function () {
+		return [
+    		'moment.js'
+		];
+	},
+
+	getStyles: function () {
+		return ["weather-icons.css", "MMM-WeatherAlerts-US.css"];
+	},
+
+	start: function () {
+		Log.info("Starting module: " + this.name);
+		this.scheduleUpdate(this.config.initialLoadDelay);
+	},
+
+
+	loaded: function(callback) {
+		this.finishLoading();
+		Log.log(this.name + ' is loaded!');
+  		callback();
+	}
+	
+	updateAlerts: function () {
+
+	var url = "https://alerts.weather.gov/cap/wwaatmget.php?x=" + this.config.location;
+	
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", url, true);
+	xhttp.onreadystatechange = function() {
+		if (this.readyState === 4) {
+			if (this.status === 200) {
+				this.requestComplete = true;
+				this.processAlert(this.responseXML);
+				this.updateDom(this.config.animationSpeed);
+			}
+			else {
+				this.failureFlag = true;
+				this.status = this.status;
+				self.updateDom(self.config.animationSpeed);
+			}
+			if (retry) {
+				self.scheduleUpdate((self.loaded) ? -1 : self.confg.retryDelay);
+			}
+		}
+		xhttp.send();
+	},
+
+	processAlerts: function (data) {
+		alerts = data.getElementsByTagName("event");
+		print alerts.length;
+	}
+
+	scheduleUpdate: function(delay) {
+		var nextLoad = this.config.updateInterval;
+		if (typeof delay !== "undefined" && delay >= 0) {
+			nextLoad = delay;
+		}
+
+		clearTimeout(this.updateTimer);
+		this.updateTimer = setTimeout(function() {
+			this.updateRequest();
+		}, nextLoad);
+	}
 });
