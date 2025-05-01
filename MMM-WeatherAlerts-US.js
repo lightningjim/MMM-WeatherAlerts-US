d0f1a7cb5cc893d906bdf0eb73cf65f5cad863b7
Kyle Creasey <kyle.creasey@dell.com>
2025-05-01 14:40:45 -0500

    Update (still not fully functional)

--- MMM-WeatherAlerts-US.js
+++ MMM-WeatherAlerts-US.js
@@ -1,105 +1,28 @@
 Module.register("MMM-WeatherAlerts-US",{
     // Default module config.
     defaults: {
-		updateInterval: 10*60*1000, // 10 minutes
-		initialLoadDelay: 250,
+		updateInterval: 15*60*1000, // 10 minutes
 		retryDelay: 2500,
 		animationSpeed: 1000,
-		location: 'OKZ029', //Cleveland Co, Oklahoma
-        text: "Loading..."
     },
-
-    // Override dom generator.
-    getDom: function() {
-        var wrapper = document.createElement("div");
-        wrapper.innerHTML = this.config.text;
-        return wrapper;
-    },
-
-	getScripts: function () {
-		return [
-		'alerts.js',
-    		'moment.js'
-		];
-	},
 
 	getStyles: function () {
 		return ["weather-icons.css", "MMM-WeatherAlerts-US.css"];
 	},
 
 	start: function () {
-		Log.info("Starting module:D " + this.name);
-		this.scheduleUpdate(this.config.initialLoadDelay);
+		Log.info("Starting module: " + this.name);
+		this.sendSocketNotification("GET_NOAA_ALERTS", { lat: this.config.lat, lon: this.config.lon});
 	},
 
-
-	loaded: function(callback) {
-		this.finishLoading();
-		Log.log(this.name + ' is loaded!');
-  		callback();
-	},
-	
-	updateAlerts: function () {
-		self = this;
-		retry = true;
-
-		var url = "https://alerts.weather.gov/cap/wwaatmget.php?x=" + this.config.location;
-		//Log.log(this.name +": url " + url);	
-		var xhttp = new XMLHttpRequest();
-		xhttp.open("GET", url, true);
-		xhttp.onreadystatechange = function() {
-			if (this.readyState === 4) {
-				if (this.status === 200) {
-					self.requestComplete = true;
-					self.processAlerts(this.responseText);
-					self.updateDom(self.config.animationSpeed);
-				}
-				else {
-					self.failureFlag = true;
-					self.status = this.status;
-					self.updateDom(self.config.animationSpeed);
-				}
-				if (retry) {
-					self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
-				}
-			}
-		};		
-		xhttp.send();
-	},
-
-	processAlerts: function (data) {
-		alertsClean = [];
-		//Log.log(this.name + ": RESPONSE - " + data);
-		var xmlDoc = new DOMParser().parseFromString(data,'text/xml');
-		//Log.log(this.name + ": DOM Parsed - " + xmlDoc);
-		alerts = xmlDoc.getElementsByTagName("entry");
-		Log.log(this.name + ": Alerts result " + alerts[0].getElementsByTagName("title")[0].childNodes[0].nodeValue);
-		alertsClean = alertsParse(alerts);
-		//this.config.text = alertsClean.length + " alerts";
-		Log.log(this.name + ": " + alertsClean.length + " alerts.");
-		if (!alertsClean[0]) { this.config.text = "No alerts." }
-		else {
-			this.config.text = "";
-			var now = new Date;
-			for(i=0; i<alertsClean.length; i++)
-			{
-				if(alertsClean.length != i+1) {newline="<br/>";}
-				else {newline="";}
-				this.config.text += alertsClean[i].type + " until " + dateTest(alertsClean[i].expires) + newline;
-			}
+	socketNotificationReceived: function (notification, payload) {
+		if(notification === "ALERTS_RECEIVED") {
+			this.alerts = payload;
+			this.updateDom();
 		}
-
 	},
 
-	scheduleUpdate: function(delay) {
-		var nextLoad = this.config.updateInterval;
-		if (typeof delay !== "undefined" && delay >= 0) {
-			nextLoad = delay;
-		}
-
-		self = this;
-		setTimeout(function() {
-			self.updateAlerts();
-		}, nextLoad);
+	getDom: function () {
+		return null;
 	}
 });
---
+++ node_helper.js
@@ -0,0 +1,34 @@
+const NodeHelper = require("node_helper");
+const fetch = (...args) => import('node-fetch').then(({ default: f }) => f(...args));
+const Log = require("logger");
+const xpath    = require("xpath");
+const { DOMParser } = require("@xmldom/xmldom");
+const select = xpath.useNamespaces({
+ 'atom': "http://www.w3.org/2005/Atom",
+ 'cap': "urn:oasis:names:tc:emergency:cap:1.2"
+});
+
+module.exports = NodeHelper.create({
+    start: function() {
+        Log.info("Starting node_helper for MMM-WeatherAlerts-US...");
+    },
+
+    socketNotificationReceived: async function(notification, payload) {
+        if (notification === "GET_NOAA_ALERTS") {
+          Log.info("WxAlerts: GET_NOAA_ALERTS")
+          const { lat, lon } = payload;
+          Log.info("WxAlerts - intermediate payload" + lat + " " + lon); 
+          const alerts = await this.getAlerts(lat, lon);
+          // Send the results back to your front-end module
+          this.sendSocketNotification("ALERTS_RECEIVED", alerts);
+        }
+    },
+
+    async getAlerts(lat, lon) {
+        const alertsUrl = `https://api.weather.gov/alerts/active.atom?point=${lat},${lon}`;
+        Log.info("WxAlerts: Alerts URL" + alertsUrl);
+        const result = await fetch(alertsUrl);
+        const doc = new DOMParser().parseFromString(result.tex, "text/xml");
+        Log.info("WxAlerts: Parsed DOM" + doc);
+    }
+});
